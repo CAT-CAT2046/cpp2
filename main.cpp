2da21a48f60714005ae21e86ecac2e6ddbbe2712
CAT-CAT2046 <tomonori0406@outlook.jp>
2022-08-26 00:32:33 +0900

    windows template

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
-#include<atcoder/all>
+#include <atcoder/all>
 #define int long long
 #define double long double
+using ll=long long;
+const int INF=1000000000000000000;
 using namespace std;
 int size(int number){
     string numstr=to_string(number);
     return numstr.size();
 }
-signed main(void){
+int op(int a,int b){
+    return a+b;
 }
-
+int e(){
+    return 0;
+}
+using namespace atcoder;
+constexpr int MOD=1000000007;
+signed main(void){
+    srand((unsigned)time(NULL));
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+} 
  
